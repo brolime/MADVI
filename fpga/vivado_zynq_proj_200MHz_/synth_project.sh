26b0528e441f19a0c99d8dc7123ee0f284964c63
Crespojohn <Crespojohn5@gmail.com>
2026-01-06 13:55:16 -0500

    added bit files for i2c with ai on fpga, ai only project on fpga

---
+++ fpga/vivado_zynq_proj_200MHz_/synth_project.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+cd /home/johncrespo/MADVI/fpga/vivado_zynq_proj_9n5w7zv_
+vivado -mode batch -source /home/johncrespo/MADVI/fpga/vivado_zynq_proj_9n5w7zv_/ip_config.tcl
+cd /home/johncrespo/MADVI/fpga/vivado_zynq_proj_9n5w7zv_
