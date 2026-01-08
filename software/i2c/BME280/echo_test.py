a205cbe4acdb472c07acc6597d8f7b1a7b1e9015
Crespojohn <Crespojohn5@gmail.com>
2026-01-08 14:00:23 -0500

    forgot to add a print statement, oops 2

--- software/i2c/BME280/echo_test.py
+++ software/i2c/BME280/echo_test.py
@@ -78,9 +78,9 @@ iic_data = [0xD0]
 read_data = []
 
 i2c_bus.send(0x77,iic_data,len(iic_data),1)
-i2c_bus.receive(0x77,rx_data,2,0)
+read_data = i2c_bus.receive(0x77,rx_data,2,0)
 
-print(rx_data)
+print(read_data)
 
 
 # -----------------------------
